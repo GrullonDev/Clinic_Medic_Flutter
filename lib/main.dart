d62b442a3c43f2507c35015d8200dfef3ad81936
GrullonDev <prosystem155@gmail.com>
2024-09-07 18:42:33 -0600

    Firebase integration into the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:proyecto_graduacion/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Determinar el entorno y cargar el archivo .env correspondiente
+  // const bool isProduction = bool.fromEnvironment('dart.vm.product');
+  // await dotenv.load(fileName: isProduction ? ".env.prod" : ".env.dev");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     const MyApp(),
