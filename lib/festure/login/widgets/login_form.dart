7c352c47f3489a32bc1b96b2f550d9fd3b75a08d
GrullonDev <prosystem155@gmail.com>
2024-10-06 19:51:11 -0600

    Improvement in the login ui form

--- lib/festure/login/widgets/login_form.dart
+++ lib/festure/login/widgets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:proyecto_graduacion/widgets/images/assets_image.dart';
 import 'package:proyecto_graduacion/widgets/routes/app_routes.dart';
 
 class LoginForm extends StatelessWidget {
@@ -7,60 +8,84 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const Flexible(
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'Correo Electrónico',
-              border: OutlineInputBorder(),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 500),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const CustomImageWidget(
+            imageUrl: 'logo3',
+            fit: BoxFit.cover,
+            height: 300,
+            width: 300,
+            isNetwork: false,
+          ),
+          const Flexible(
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Correo Electrónico',
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(fontSize: 20),
             ),
           ),
-        ),
-        const SizedBox(height: 16.0),
-        const Flexible(
-          child: TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Contraseña',
-              border: OutlineInputBorder(),
+          const SizedBox(height: 16.0),
+          const Flexible(
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(fontSize: 20),
             ),
           ),
-        ),
-        const SizedBox(height: 16.0),
-        Align(
-          alignment: Alignment.centerRight,
-          child: TextButton(
+          const SizedBox(height: 16.0),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {
+                // Aquí se manejará la lógica para recuperación de contraseña.
+              },
+              child: const Text(
+                '¿Olvidaste tu contraseña?',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          ElevatedButton(
             onPressed: () {
-              // Aquí se manejará la lógica para recuperación de contraseña.
+              context.go(AppRoutes.home);
             },
-            child: const Text('¿Olvidaste tu contraseña?'),
+            child: const Text(
+              'Iniciar Sesión',
+              style: TextStyle(fontSize: 20),
+            ),
           ),
-        ),
-        const SizedBox(height: 16.0),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                context.go(AppRoutes.home);
-              },
-              child: const Text('Iniciar Sesión'),
-            ),
-            const SizedBox(height: 30),
-            TextButton(
-              onPressed: () {
-                // Aquí se manejará la lógica para registrarse.
-              },
-              child: const Text('¿No tienes cuenta? Regístrate'),
-            ),
-          ],
-        ),
-      ],
+          const SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '¿No tienes cuenta?',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Aquí se manejará la lógica para registrarse.
+                },
+                child: const Text(
+                  ' Regístrate',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/images/assets_image.dart
+++ lib/widgets/images/assets_image.dart
@@ -3,28 +3,34 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomImageWidget extends StatelessWidget {
   final String imageUrl;
+  final bool isNetwork;
+  final double height;
+  final double width;
+  final BoxFit fit;
 
   const CustomImageWidget({
     super.key,
     required this.imageUrl,
+    this.isNetwork = true,
+    this.height = 450,
+    this.width = 450,
+    required this.fit,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (imageUrl.isNotEmpty) {
-      return Image.network(
+    if (isNetwork) {
+      // Si la imagen es remota (de la red)
+      return SvgPicture.network(
         imageUrl,
-        height: 25,
-        width: 25,
-        fit: BoxFit.cover,
+        height: height,
+        width: width,
+        fit: fit,
       );
     } else {
-      return SvgPicture.asset(
-        'assets/profile.svg',
-        height: 25,
-        width: 25,
-        fit: BoxFit.cover,
-      );
+      // Si la imagen es local (de los assets)
+      return SvgPicture.asset('assets/$imageUrl.svg',
+          height: height, width: width, fit: fit);
     }
   }
 }
